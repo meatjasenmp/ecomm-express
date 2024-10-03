afa0b3180e30cecdba695d1719e78eb61dec8d6d
Jasen Peterson <me@jasenmp.com>
2024-10-03 18:45:49 -0500

    Connecting to atlas DB. WIP.

--- app/routes/public.ts
+++ app/routes/public.ts
@@ -1,14 +1,14 @@
-import express, { type Response } from 'express';
+import express, { type Request, type Response } from 'express';
 import db from '../db/conn';
 import { PUBLIC_ROUTES } from './routes';
 
 const publicRouter = express.Router();
 
-publicRouter.get(`${PUBLIC_ROUTES.HOME}`, async (res: Response): Promise<void> => {
+publicRouter.get(`${PUBLIC_ROUTES.HOME}`, async (_: Request, res: Response): Promise<void> => {
   const collection = db?.collection('comments');
   const results = await collection?.find({}).limit(10).toArray();
 
-  console.info('Results:', results);
+  res.send(results);
 });
 
 export default publicRouter;
