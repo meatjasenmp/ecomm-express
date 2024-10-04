f168b797dff4a7dcbe3e6da15f26655d1368a953
Jasen Peterson <me@jasenmp.com>
2024-10-04 07:25:15 -0500

    Init mongoose.

--- app/routes/public.ts
+++ app/routes/public.ts
@@ -5,7 +5,7 @@ import { PUBLIC_ROUTES } from './types.ts';
 const publicRouter = express.Router();
 
 publicRouter.get(`${PUBLIC_ROUTES.HOME}`, async (_: Request, res: Response): Promise<void> => {
-  const collection = db?.collection('comments');
+  const collection = db?.collection('products');
   const results = await collection?.find({}).limit(10).toArray();
 
   res.send(results);
