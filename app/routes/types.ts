ddd117c4512f7dc8bbd6cc089c8f987ec032fbbb
Jasen Peterson <me@jasenmp.com>
2024-10-04 08:00:42 -0500

    Route for product creation.

--- app/routes/types.ts
+++ app/routes/types.ts
@@ -4,6 +4,8 @@ const enum PUBLIC_ROUTES {
 
 const enum ADMIN_ROUTES {
   CREATE_PRODUCTS = '/create-products',
+  DELETE_PRODUCTS = '/delete-products',
+  UPDATE_PRODUCTS = '/update-products',
 }
 
 export { PUBLIC_ROUTES, ADMIN_ROUTES };
