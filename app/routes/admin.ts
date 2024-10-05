ab4ce5373e5187f0639ae2a4308a1e9d1449d670
Jasen Peterson <me@jasenmp.com>
2024-10-05 07:05:02 -0500

    Cleaning up.

--- app/routes/admin.ts
+++ app/routes/admin.ts
@@ -17,24 +17,21 @@ adminRouter.post('/create-product', async (req: Request<ProductInterface>, res: 
   }
 });
 
-adminRouter.patch(
-  '/update-product/products/:id',
-  async (req: Request<ProductRequest>, res: Response): Promise<void> => {
-    const { id } = req.params;
+adminRouter.patch('/update-product/:id', async (req: Request<ProductRequest>, res: Response): Promise<void> => {
+  const { id } = req.params;
 
-    try {
-      const updatedProduct = await Product.updateOne({ _id: id }, req.body);
-      res.json(updatedProduct).status(200);
-    } catch (err) {
-      res.status(400).send({
-        message: 'Failed to update product',
-        error: err,
-      });
-    }
-  },
-);
+  try {
+    const updatedProduct = await Product.updateOne({ _id: id }, req.body);
+    res.json(updatedProduct).status(200);
+  } catch (err) {
+    res.status(400).send({
+      message: 'Failed to update product',
+      error: err,
+    });
+  }
+});
 
-adminRouter.delete('/delete-product/products/:id', async (req: Request, res: Response): Promise<void> => {
+adminRouter.delete('/delete-product/:id', async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
 
   try {
