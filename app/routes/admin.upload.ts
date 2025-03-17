364a7303c5046c81ea1b4279f7d4c453383a8cbf
Jasen Peterson <me@jasenmp.com>
2025-03-17 10:24:08 -0500

    WIP. Image parsing

--- app/routes/admin.upload.ts
+++ app/routes/admin.upload.ts
@@ -6,7 +6,10 @@ const uploadRoutes = express.Router();
 
 uploadRoutes.put('/upload-images', upload.array('images', 10), async (req: Request, res: Response): Promise<void> => {
   try {
-    res.json({ message: 'Images uploaded successfully' }).status(200);
+    const files = req?.files as Express.MulterS3.File[];
+    const urls = files.map((file) => file.location);
+    console.log(urls);
+    res.json({ message: 'Images uploaded successfully' }).status(201);
   } catch (err) {
     res.status(400).send({
       message: 'Failed to upload images',
@@ -17,7 +20,7 @@ uploadRoutes.put('/upload-images', upload.array('images', 10), async (req: Reque
 
 uploadRoutes.put('/upload-video', upload.single('video'), async (_req: Request, res: Response): Promise<void> => {
   try {
-    res.json({ message: 'Video uploaded successfully' }).status(200);
+    res.json({ message: 'Video uploaded successfully' }).status(201);
   } catch (err) {
     res.status(400).send({
       message: 'Failed to upload video',
