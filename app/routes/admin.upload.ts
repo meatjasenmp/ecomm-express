d4addad04e5fe2a082f2e28d9b1d521ff29af034
Jasen Peterson <me@jasenmp.com>
2025-05-28 07:31:44 -0500

    Changing this to single image upload. For now.

--- app/routes/admin.upload.ts
+++ app/routes/admin.upload.ts
@@ -4,17 +4,13 @@ import upload from '../helpers/s3';
 
 const uploadRoutes = express.Router();
 
-uploadRoutes.put('/upload-images', upload.array('images', 10), async (req: Request, res: Response): Promise<void> => {
+uploadRoutes.put('/upload-image', upload.single('image'), async (req: Request, res: Response): Promise<void> => {
   try {
-    const files = req?.files as Express.MulterS3.File[];
-    const urls = files.map((file) => file.location);
-    console.log(urls);
-    res.json({ message: 'Images uploaded successfully' }).status(201);
+    const file = req.file as Express.MulterS3.File;
+    console.info('File:', file);
+    res.json({ message: 'Image uploaded successfully' }).status(201);
   } catch (err) {
-    res.status(400).send({
-      message: 'Failed to upload images',
-      error: err,
-    });
+    res.status(400).send({ message: 'Failed to upload image', error: err });
   }
 });
 
@@ -22,9 +18,6 @@ uploadRoutes.put('/upload-video', upload.single('video'), async (_req: Request, 
   try {
     res.json({ message: 'Video uploaded successfully' }).status(201);
   } catch (err) {
-    res.status(400).send({
-      message: 'Failed to upload video',
-      error: err,
-    });
+    res.status(400).send({ message: 'Failed to upload video', error: err });
   }
 });
