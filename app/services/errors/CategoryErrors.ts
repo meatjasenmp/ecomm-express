9ca69fca6a783791be5c76aca81d3227a03427ed
meatjasenmp <me@jasenmp.com>
2025-07-24 06:17:38 -0500

    Adding a BaseErrors.ts so we can use it multiple scenarios. ie: CategoryErrors and ProductErrors.

--- app/services/errors/CategoryErrors.ts
+++ app/services/errors/CategoryErrors.ts
@@ -1,18 +1,20 @@
-export class CategoryError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'CategoryError';
+import { ResourceError, NotFoundError } from './BaseErrors';
+
+export class CategoryError extends ResourceError {
+  constructor(message: string, statusCode = 400) {
+    super('Category', message, statusCode);
   }
 }
 
-export class CategoryNotFoundError extends CategoryError {
+export class CategoryNotFoundError extends NotFoundError {
   constructor(id: string) {
-    super(`Category with ID ${id} not found`);
+    super('Category', id);
   }
 }
 
 export class InvalidHierarchyError extends CategoryError {
   constructor(message: string) {
     super(message);
+    this.name = 'InvalidHierarchyError';
   }
 }
