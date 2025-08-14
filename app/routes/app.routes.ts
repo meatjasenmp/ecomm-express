84f9082b4f656a09ad9b2009c69ec02266bbd05b
meatjasenmp <me@jasenmp.com>
2025-08-14 04:45:46 -0500

    Deleting old routes.

--- app/routes/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import productRoutes from './products/index.ts';
-import categoryRoutes from './categories/index.ts';
-import imageRoutes from './images/index.ts';
-
-const router = express.Router();
-
-export default router;
